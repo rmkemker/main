146e73d6057c46f5d2aa7e57a1e8504510c2c787
Ronald Kemker <rmk6217@rit.edu>
2017-05-10 08:49:43 -0400

    Added an example for the Parallelizer class.

---
+++ examples/parallelizer_example.py
@@ -0,0 +1,26 @@
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.datasets import mnist
+from keras.utils import to_categorical
+from machine_learning.parallelizer import Parallelizer
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = x_train.reshape(60000, -1)
+x_test = x_test.reshape(10000, -1)
+model = Sequential()
+model.add(Dense(64, input_shape=(784,), activation='relu'))
+model.add(Dense(10, activation='softmax'))
+
+para = Parallelizer()
+parallel_model = para.transform(model)
+
+batch_size = 128 * para.n_gpus
+
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+parallel_model.compile(optimizer='nadam', loss='categorical_crossentropy',
+                       metrics=['accuracy'])
+
+parallel_model.fit(x_train, y_train, batch_size=batch_size,
+                   validation_data=(x_test, y_test))
